1f5ffdc3905ebf6133a05ed624e88a88bd2c0ad2
ibrahimsuhail <45448860+ibrahimsuhail@users.noreply.github.com>
2021-03-03 17:33:23 -0500

    Update image_recognition_aws.py

--- image_recognition_aws.py
+++ image_recognition_aws.py
@@ -4,9 +4,9 @@ import boto3
 def lambda_handler(event, context):
     rekognition = boto3.client("rekognition")
     s3 = boto3.client("s3")
-    fileObj = s3.get_object(Bucket= "ibisbucket", Key="Coffee-in-a-cup.jpg" )
+    fileObj = s3.get_object(Bucket= "<BUCKET NAME>", Key="<NAME OF OBJECT IN YOUR BUCKET>" )
     file_content = fileObj["Body"].read()
-    response = rekognition.detect_labels(Image= {"S3Object" : {"Bucket" : "ibisbucket", "Name": "Coffee-in-a-cup.jpg"}}, MaxLabels=3, MinConfidence=70 )
+    response = rekognition.detect_labels(Image= {"S3Object" : {"Bucket" : "ibisbucket", "<BUCKET NAME>": "<NAME OF OBJECT IN YOUR BUCKET>"}}, MaxLabels=3, MinConfidence=70 )
     print(response)
     return {
         'statusCode': 200,
